b32b9eb443014534555d0d6e74cc4e058e874b45
Derrik Milligan <derrik.milligan.dev@gmail.com>
2021-11-06 22:37:10 -0600

    Got Bootstrap setup and some initial authentication stuff... It needs a lot of work though.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,35 @@
+
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	import { userNeedsToLogin } from '$lib/guards';
+
+	// Get the Header component so we can display our nav
+	import Navigation from '$lib/Navigation.svelte';
+
+	// Import all the bootstrap styles
+	import 'bootstrap/dist/css/bootstrap.min.css';
+
+	// Import our global styles
 	import '../app.css';
+
+	onMount(async () => {
+		// When we load a page check to see if we need to be logged in before accessing it
+		if (await userNeedsToLogin({ page: $page })) {
+			await goto('/login');
+		}
+	});
 </script>
 
-<Header />
+
+<Navigation />
 
 <main>
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
 
 <style>
 	main {
@@ -23,23 +41,5 @@
 		max-width: 1024px;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
 	}
 </style>
