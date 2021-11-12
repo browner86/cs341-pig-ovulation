a5d64ef943943e5c69fbd1ba65f51e6d723d3528
browner86 <50446300+browner86@users.noreply.github.com>
2021-11-11 17:54:42 -0700

    add styling to pig register

--- src/routes/pigs/new.svelte
+++ src/routes/pigs/new.svelte
@@ -1,19 +1,32 @@
 <svelte:head>
-  <title>Register Pig</title>
+	<title>Register Pig</title>
 </svelte:head>
 
 <section>
-  <form>
-    <label for="name">Pig Name</label><br>
-    <input type="text" name="name" id="name" placeholder="Wilbur"><br>
-    <label for="description">Description</label><br>
-    <textarea name="desctiption" id="description" cols="30" rows="3" placeholder="spot below left ear"></textarea><br>
-    <label for="breed">Breed</label><br>
-    <input type="text"><br>
-    <label for="dob"> Aproximate Date of Birth</label><br>
-    <input type="date"><br>
-    <button>Add Pig to Farm</button>
-  </form>
+	<form>
+		<div class="form-group">
+			<label for="name">Pig Name</label><br />
+			<input type="text" class="form-control" name="name" id="name" placeholder="Wilbur" /><br />
+		</div>
+		<div class="form-group">
+			<label for="description">Description</label>
+			<textarea
+				class="form-control"
+				name="desctiption"
+				id="description"
+				cols="30"
+				rows="3"
+				placeholder="spot below left ear"
+			/><br />
+		</div>
+		<div class="form-group">
+			<label for="breed">Breed</label>
+			<input type="text" class="form-control" /><br>
+		</div>
+		<div class="form-group">
+			<label for="dob"> Aproximate Date of Birth</label>
+			<input type="date" class="form-control"/><br />
+		</div>
+		<button type="submit" class="btn btn-primary">Add Pig to Farm</button>
+	</form>
 </section>
-
-
