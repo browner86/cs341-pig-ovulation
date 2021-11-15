176c7193027c321d7a1cac79a6324b450bd84d9d
browner86 <50446300+browner86@users.noreply.github.com>
2021-11-15 16:59:34 -0700

    add pig model

---
+++ src/lib/models/Pig.ts
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { v4 as uuid } from 'uuid';
+
+// Create the schema
+export const pigSchema = new mongoose.Schema({
+    name:   String,
+    description: String,
+    breed: String,
+    dob: Date,
+  });
+  
+  // Create the actual model to use
+export const Pig = mongoose.model('Pig', pigSchema);
+// Export all the methods as a singleton to use with the user
+export default {
+      // Method to retreive a pig by id
+  get: async (id: Number) => {
+    return await Pig.findById(id).exec();
+  },
+
+  
+  // Create a new pig
+  create: async ({ name, description ='', breed ='', dob }) => {
+    const pig = new Pig({
+      name,
+      description,
+      breed,
+      dob,
+    });
+
+    await pig.save();
+
+    return pig;
+  },
+}
